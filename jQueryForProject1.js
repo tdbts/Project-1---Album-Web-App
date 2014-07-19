eb3eb0a8e71b779801a3ffb9094a73853f093e04
tdbts <VinnySanchez87@gmail.com>
2014-07-19 00:00:20 -0400

    goodnight y'all...tomorrow's another sonovabitch

--- jQueryForProject1.js
+++ jQueryForProject1.js
@@ -2,5 +2,16 @@ $(document).ready(function() {
 
     $("#header").hide();
     $("#header").fadeIn(1000);
+
+    $("#searchButton").click(function() {
+    	
+    	$("#results").css("margin-Left", "50px");
+    	$("#results").hide();
+    	$("#results").fadeIn(1000);
+
+    	$("#results").animate({marginLeft: "700px"});
+    	    
+    });
 	
 });
+
