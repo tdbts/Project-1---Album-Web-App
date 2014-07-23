bdfc03ea2ce7032a1429cd4bb5265ea80cd2e398
tdbts <VinnySanchez87@gmail.com>
2014-07-23 11:17:50 -0400

    Yeahhhh baby...Got the info for each album to display

--- jQueryForProject1.js
+++ jQueryForProject1.js
@@ -12,7 +12,23 @@ $(document).ready(function() {
     	$("#results").css("margin-Left", "50px");
     	$("#results").fadeIn(600).animate({marginLeft: "700px"}, 250, function() {
 
+            $.getJSON('albumsJSON.json', function(albumsJSON) {
+                var albumField = document.getElementById('albums');
+                var s = albumField.selectedIndex;
+                var album = albumField.options[s].value;
+                console.log(album);
+
+                var selectedAlbum = albumsJSON.Albums_Needed[album - 1];
+
+                $("#title").html(selectedAlbum.Title);
+                $("#artist").html(selectedAlbum.Artist);
+                $("#year").html(selectedAlbum.Year);
+                $("#genre").html(selectedAlbum.Genre);
+                $("#lifeDetail").html(selectedAlbum.Life_Detail);
+
     		$("#albumInfo").fadeIn(500);
+
+            });
     		
     	});
 
